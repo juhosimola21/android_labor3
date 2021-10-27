eaa3932e4ed2066a5bcbfe70d8dc5c78bde71ad5
juhosimola21 <79316712+juhosimola21@users.noreply.github.com>
2021-10-27 09:30:51 +0300

    QuizAPP

---
+++ fel2/app/src/main/java/com/example/lab3/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.lab3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener { view ->
+            val toast = Toast.makeText(applicationContext, "Loads data...", Toast.LENGTH_SHORT)
+            toast.show()
+            Log.i("Info", "Button Pressed");
+        }
+    }
+
+}
